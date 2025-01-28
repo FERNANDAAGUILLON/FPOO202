1fb02b1732b95457bff6282510b2d5a5afa82234
FERNANDAAGUILLON <123046536@upq.edu.mx>
2025-01-28 15:34:23 -0600

    ejercicio 6 terminado

--- proyecto 1/src/ejercicio6.java
+++ proyecto 1/src/ejercicio6.java
@@ -5,12 +5,12 @@ public class ejercicio6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("sabe que hace una vaca en la calle - va caminando: ");
+        System.out.print("ingrese una frase: ");
         String frase = scanner.nextLine();
 
         String fraseInvertida = new StringBuilder(frase).reverse().toString();
 
-        System.out.println("sabe que hace una vaca en la calle - va caminando: " + fraseInvertida);
+        System.out.println("ingrese una frase: " + fraseInvertida);
 
         scanner.close();
     }
