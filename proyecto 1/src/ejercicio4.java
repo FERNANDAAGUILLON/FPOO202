c52d1fa748add58863572c7149586c6ed64ba9f2
FERNANDAAGUILLON <123046536@upq.edu.mx>
2025-01-28 15:19:17 -0600

    ejercicio 4 terminado

---
+++ proyecto 1/src/ejercicio4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ejercicio4 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce tu nombre: ");
+        String nombre = scanner.nextLine();
+
+        String nombreEnMayusculas = nombre.toUpperCase();
+        int numeroLetras = nombre.replace(" ", "").length();
+
+        System.out.println(nombreEnMayusculas + " tiene " + numeroLetras + " letras");
+
+        scanner.close();
+    }
+}
+
+
