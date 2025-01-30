abe47bb353bb725e00356bb90f38603ae3c5241b
FERNANDAAGUILLON <123046536@upq.edu.mx>
2025-01-30 15:27:34 -0600

    ejercicio 1 terminado

---
+++ proyecto 2/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args){
+
+                String passwordGuardada = "246810";
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Introduce tu contraseña: ");
+                String passwordUsuario = scanner.nextLine();
+
+                if (passwordGuardada.equalsIgnoreCase(passwordUsuario)) {
+                    System.out.println("La contraseña coincide.");
+                } else {
+                    System.out.println("La contraseña no coincide.");
+                }
+
+                scanner.close();
+            }
+        }
+
+
+
---
+++ proyecto 2/src/ejercicio3.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class ejercicio3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("introduce la edad del cliente: ");
+        int edad = scanner.nextInt();
+
+        if (edad < 4) ;
+        {
+            System.out.print("entrada gartis. ");
+
+        }  if (edad >= 4 && edad <= 18){
+
+            System.out.print(" El precio de la entrada 110. ");
+
+            } else {
+
+            System.out.print(" el precio de la entrada 190. ");
+
+        }
+
+        scanner.close();
+    }
+}
+
+
+
+
