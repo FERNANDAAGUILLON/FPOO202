33736ae460955c61b4f515cbadd0c7c6c7d196fa
FERNANDAAGUILLON <123046536@upq.edu.mx>
2025-02-04 14:57:09 -0600

    2 EJERCICIO

--- EXAMENPC/src/PATE2.java
+++ EXAMENPC/src/PATE2.java
@@ -1,13 +1,18 @@
 //programa que solicite una palabra y que la descomponga en letras
+
 import java.util.Scanner;
+
 public class PATE2 {
 
         public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
+
             System.out.print("Introduce una palabra: ");
             String palabra = scanner.nextLine();
+
             char[] letras = palabra.toCharArray();
             System.out.println("Letras de la palabra:");
+            
             for (char letra : letras) {
                 System.out.println(letra);
             }
@@ -15,6 +20,12 @@ public class PATE2 {
             scanner.close();
         }
     }
+
+
+
+
+
+
 
 
 
