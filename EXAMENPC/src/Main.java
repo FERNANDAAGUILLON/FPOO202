27b057d02316ae7bbd0f25be663650beaeb25977
FERNANDAAGUILLON <123046536@upq.edu.mx>
2025-02-04 14:49:39 -0600

    segundo ejercicio terminado

---
+++ EXAMENPC/src/Main.java
@@ -0,0 +1,31 @@
+// escriba un programa que pida al usuario dos palabras,y que indique cual es mas largas y cuantas palabras es mas larga
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Introduce la primera palabra: ");
+        String palabra1 = scanner.nextLine();
+
+        System.out.print("Introduce la segunda palabra: ");
+        String palabra2 = scanner.nextLine();
+
+
+        int longitud1 = palabra1.length();
+        int longitud2 = palabra2.length();
+
+
+        if (longitud1 > longitud2) {
+            int diferencia = longitud1 - longitud2;
+            System.out.println("La palabra '" + palabra1 + "' es más larga por " + diferencia + " letras.");
+        } else if (longitud2 > longitud1) {
+            int diferencia = longitud2 - longitud1;
+            System.out.println("La palabra '" + palabra2 + "' es más larga por " + diferencia + " letras.");
+        } else {
+
+
+        }
+    }
+}
---
+++ EXAMENPC/src/PATE2.java
@@ -0,0 +1,22 @@
+//programa que solicite una palabra y que la descomponga en letras
+import java.util.Scanner;
+public class PATE2 {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Introduce una palabra: ");
+            String palabra = scanner.nextLine();
+            char[] letras = palabra.toCharArray();
+            System.out.println("Letras de la palabra:");
+            for (char letra : letras) {
+                System.out.println(letra);
+            }
+
+            scanner.close();
+        }
+    }
+
+
+
+
+
