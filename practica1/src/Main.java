03300c63f985c1e351c86414b5961958d93de8c8
FERNANDAAGUILLON <123046536@upq.edu.mx>
2025-02-18 15:42:13 -0600

    Trabajo terminado

---
+++ practica1/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.Callable;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+// creamos el primer objeto
+Spartan masterChief= new Spartan();
+
+// usar atributos
+        masterChief.nombre= "john"  ;
+        masterChief.salud=100;
+        masterChief.escudo=70;
+        masterChief.armaPrincipal="Rifle de asalto";
+
+// invocamos los metodos
+        masterChief.mostrarinfo();
+        masterChief.atacar("Grunt");
+        masterChief.recargarArma(75);
+        masterChief.correr(true);
+
+
+
+            // creamos el primer objeto
+            Spartan Chef = new Spartan();
+
+            // usar atributos
+            Chef.nombre= " pedro"  ;
+            Chef.salud=100;
+            Chef.escudo=70;
+           Chef.armaPrincipal="Espatula";
+
+// invocamos los metodos
+            Chef.mostrarinfo();
+            Chef.atacar("michel");
+            Chef.recargarArma(75);
+            Chef.correr(true);
+
+        }
+    }
---
+++ practica1/src/Spartan.java
@@ -0,0 +1,39 @@
+public class Spartan {
+
+    public String nombre;
+    int escudo, salud;
+    String armaPrincipal;
+
+    public void mostrarinfo() {
+        System.out.println("-----------Informacion del Spartan-----------");
+        System.out.println("Nombe :" + nombre);
+        System.out.println("% de salud :" + salud);
+        System.out.println("% de Escudo :" + escudo);
+        System.out.println("Arma disponible :" + armaPrincipal);
+        System.out.println("-----------------------------------------------");
+
+    }
+
+    public void atacar(String enemigo) {
+        System.out.println(nombre + "atac a" + enemigo + "con" + armaPrincipal);
+    }
+
+    public void recargarArma(int municiones) {
+        int restantes = 10;
+        int total = restantes + municiones;
+        System.out.println(armaPrincipal + " ahora tiene disponible" + total + "balas");
+    }
+
+    public void correr(boolean status) {
+        if (status) {
+            System.out.println(nombre + "esta corriendo");
+            {
+                System.out.println(nombre + "se detuvo");
+
+
+
+
+            }
+        }
+    }
+}
