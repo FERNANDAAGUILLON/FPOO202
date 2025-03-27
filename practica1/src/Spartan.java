c784c63b590b282e7a2f14dd6ecedaea25252741
FERNANDAAGUILLON <123046536@upq.edu.mx>
2025-03-27 15:44:54 -0600

    terminado

--- practica1/src/Spartan.java
+++ practica1/src/Spartan.java
@@ -49,38 +49,7 @@ public class Spartan {
 
 
 
-    public void mostrarinfo() {
-        System.out.println("-----------Informacion del Spartan-----------");
-        System.out.println("Nombe :" + nombre);
-        System.out.println("% de salud :" + salud);
-        System.out.println("% de Escudo :" + escudo);
-        System.out.println("Arma disponible :" + armaPrincipal);
-        System.out.println("-----------------------------------------------");
-
-    }
-
-    public void atacar(String enemigo) {
-        System.out.println(nombre + "atac a" + enemigo + "con" + armaPrincipal);
-    }
-
-    public void recargarArma(int municiones) {
-        int restantes = 10;
-        int total = restantes + municiones;
-        System.out.println(armaPrincipal + " ahora tiene disponible" + total + "balas");
-    }
-
-    public void correr(boolean status) {
-        if (status) {
-            System.out.println(nombre + "esta corriendo");
-            {
-                System.out.println(nombre + "se detuvo");
-
-
-            }
-
-            }
-        }
-    }
+    
 
 
 // zona de gets y sets
